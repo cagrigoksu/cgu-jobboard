edcfec766545e583acb682d66a0f1dc907b71431
cagri Goksu <cagrigoksu.ustundag@gmail.com>
2024-04-07 06:27:28 +0200

    Repository structure added.

--- JobBoard/JobBoardTest/UserControllerTest.cs
+++ JobBoard/JobBoardTest/UserControllerTest.cs
@@ -7,23 +7,23 @@ namespace JobBoardTest
 {
     public class UserControllerTest
     {
-        private readonly UserController _controller;
-        private readonly ILogger<HomeController> _logger;
-        private readonly AppDbContext DB;
-
-        public UserControllerTest(ILogger<HomeController> logger, AppDbContext context)
-        {
-            DB = context;
-            _logger = logger;
-            _controller = new UserController(_logger, DB);
-        }
-
-
-        [Fact]
-        public void Test1()
-        {
-            var result = _controller.LogIn();
-
-        }
+        // // private readonly UserController _controller;
+        // // private readonly ILogger<HomeController> _logger;
+        // // private readonly AppDbContext DB;
+        //
+        // public UserControllerTest(ILogger<HomeController> logger, AppDbContext context)
+        // {
+        //     DB = context;
+        //     _logger = logger;
+        //     _controller = new UserController(_logger, DB);
+        // }
+        //
+        //
+        // [Fact]
+        // public void Test1()
+        // {
+        //     var result = _controller.LogIn();
+        //
+        // }
     }
 }
